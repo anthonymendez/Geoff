3f848214d33e2d9229115456230abdc5c978a865
Anthony Mendez <anthonymendez9@gmail.com>
2019-08-25 23:35:58 -0400

    Properly added gitignore and all files

---
+++ RLBotCSharpExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using RLBotDotNet;
+using System.IO;
+
+namespace Geoff
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Read the port from port.cfg.
+            const string file = "port.cfg";
+            string text = File.ReadAllLines(file)[0];
+            int port = int.Parse(text);
+
+            // BotManager is a generic which takes in your bot as its T type.
+            BotManager<GeoffBot> botManager = new BotManager<GeoffBot>();
+            // Start the server on the port given in the port.cfg file.
+            botManager.Start(port);
+        }
+    }
+}
